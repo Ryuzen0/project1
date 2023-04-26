94bbec68bb9f4f9bed805cc348d5e209c4f9ba2b
Ryuzen0 <tommysatrio36@gmail.com>
2023-04-27 00:01:27 +0700

    meme

---
+++ awas.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Meme</title>
+</head>
+<body>
+	<div class="chromestyle" id="chromemenu">
+		<ul>
+			<li><a href="https://www.youtube.com/shorts/s9C03DiBU1A">Home</a></li>
+            <li><a href="https://www.youtube.com/watch?v=Gnm3hIcjiCQ" rel="dropmenu1">Resources</a></li>
+            <li><a href="https://www.youtube.com/watch?v=0fekTeVXMCM" rel="dropmenu2">News</a></li>
+            <li><a href="https://www.youtube.com/shorts/w9gyKjyS_9Q" rel="dropmenu3">Search</a></li>
+		</ul>
+
+</body>
